03ea53f68aa924a92a0dec014e688a699cdf39d6
Pavel <piv@sitecore.net>
2017-08-18 13:10:35 +0300

    Fix issue by calling base.GetResult method instead of returning null in case if this.Value of the TelephoneField class is null

---
+++ src/Sitecore.Support.179427/Forms/Mvc/ViewModels/Fields/TelephoneField.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.Support.Forms.Mvc.ViewModels.Fields
+{
+  #region Usings
+
+  using System.ComponentModel.DataAnnotations;
+  using System.Linq;
+  using Sitecore.Forms.Mvc.Validators;
+  using Sitecore.Forms.Mvc.ViewModels.Fields;
+  using WFFM.Abstractions.Actions;
+
+  #endregion
+
+  public class TelephoneField : SingleLineTextField
+  {
+    public override ControlResult GetResult()
+    {
+      return Value == null ? base.GetResult() : new ControlResult(FieldItemId, Title, new string(Value.Where(char.IsDigit).ToArray()), string.IsNullOrEmpty(Value) ? null : string.Join(string.Empty, "<scfriendly>", Value, "</scfriendly>"));
+    }
+
+    [DynamicRegularExpression(@"^\+?\s{0,}\d{0,}\s{0,}(\(\s{0,}\d{1,}\s{0,}\)\s{0,}|\d{0,}\s{0,}-?\s{0,})\d{2,}\s{0,}-?\s{0,}\d{2,}\s{0,}(-?\s{0,}\d{2,}|\s{0,})\s{0,}$", null, ErrorMessage = "The {0} field contains an invalid telephone number."), DataType(DataType.PhoneNumber)]
+    public override string Value { get; set; }
+  }
+}
---
+++ src/Sitecore.Support.179427/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.179427")]
+[assembly: AssemblyProduct("Sitecore.Support.179427")]
+[assembly: ComVisible(false)]
